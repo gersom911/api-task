32ed25d2d71141c4741578f9818cbd06584b4afd
gersom911 <familiacedram@gmail.com>
2023-03-24 06:50:54 -0500

    %jefreyLaRosa1233%

--- app/api/urls.py
+++ app/api/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from drf_spectacular.views import SpectacularAPIView,SpectacularSwaggerView,SpectacularRedocView
+
 from rest_framework import routers
 from rest_framework_extensions.routers import ExtendedSimpleRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
@@ -12,10 +12,7 @@ router.register('task', TaskViewSet)
 urlpatterns = [
     
         # YOUR PATTERNS
-    path('schema/', SpectacularAPIView.as_view(), name='schema'),
-    # Optional UI:
-    path('schema/swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger'),
-    path('schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+   
     #path('schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),  
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),      
